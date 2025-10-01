8805437690da37f78900e9525bc1f82fe5b3d6d2
Aryan Aligeti <aryanaligeti@gmail.com>
2025-10-01 10:40:05 +0530

    cleaned up files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
 import path from "path";
 import { defineConfig } from "vite";
 
@@ -49,7 +48,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
